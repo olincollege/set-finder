68d7c9499bc7c618ca1964f11b998fdc5e8a2886
TinyTinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2023-04-21 13:14:19 -0400

    Outline view.py

---
+++ view.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+class View:
+    def __init__(self, image):
+        self.image = image
+    
+    def draw_nonset_cards(list_cards):
+        pass
+    def draw_set_cards(list_cards):
+        pass 
+
+    def _draw_rectangle(card, color):
+        pass
+    
+    def new_image():
+        pass
+
+    def show(self):
+        pass
