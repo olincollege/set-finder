2843203b0397e6d1db7ba898b9b126dba51946fd
= <=>
2023-04-25 14:06:51 -0400

    Change View.py to RGB color space

--- View.py
+++ View.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 from Card import Card
 
-
 class View:
     """
     Class for displaying Set boards.
@@ -15,12 +14,11 @@ class View:
     Attributes:
         image: A cv2.Mat type image showing the full board
     """
-
     def __init__(self, image):
         self.image = image
         self._drawn_cards = []
 
-    def draw_nonset_cards(self, list_cards):
+    def draw_nonset_cards(self,list_cards):
         """
         Draws cards on the image in black.
 
@@ -28,22 +26,21 @@ class View:
             list_cards: A list of Card objects representing the nonset cards.
         """
         for card in list_cards:
-            self._draw_rectangle(card, (255, 255, 255))
+            self._draw_rectangle(card,(255,255,255))
 
-    def draw_set_cards(self, list_cards):
+    def draw_set_cards(self,list_cards):
         """
         Draws cards on the image in green.
 
         Args:
-            list_cards: A list of Card objects representing the set cards.
+            list_cards: A list of list of Card objects representing the set cards.
         """
-
+        i = 0
         for set in list_cards:
-            self._draw_rectangle(set[0], (0, 255, 0))
-            self._draw_rectangle(set[1], (0, 255, 0))
-            self._draw_rectangle(set[2], (0, 255, 0))
+            for card in set: self._draw_rectangle(card,(0,255,20*i))
+            i += 1
 
-    def _draw_rectangle(self, card: Card, color):
+    def _draw_rectangle(self,card: Card, color):
         """
         Draws a contour on the image.
 
@@ -51,7 +48,7 @@ class View:
             card: A Card object.
             color: A tuple of the color to draw the rectangle.
         """
-        cv2.drawContours(self.image, [card.contour], 0, color, 3)
+        cv2.drawContours(self.image,[card.contour],0,color,3)
 
     def new_image():
         """'
@@ -64,7 +61,7 @@ class View:
         Display board state to the user.
         """
         fig = plt.figure()
-        plt.imshow(self.image)
+        plt.imshow(cv2.cvtColor(self.image,cv2.COLOR_BGR2RGB))
         axes = plt.axes([0.81, 0.000001, 0.1, 0.075])
         bnext = Button(axes, "New")
         bnext.on_clicked(self.new_image)
