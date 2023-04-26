01354e1844c2953fd1a22a22b1568e47f8197cfb
Ari Bobesh <ari.bobesh@gmail.com>
2023-04-26 00:10:55 -0400

    Small performance improvements to Card

--- Card.py
+++ Card.py
@@ -25,64 +25,64 @@ class Card:
         return self._comparative
 
     def classify(self):
-        im = self._im
-        height, width, _ = im.shape
-        im = cv.resize(im, (250, 120))
+        # im = self._im
+        # height, width, _ = im.shape
+        # im = cv.resize(im, (250, 120))
 
-        # hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
-        # greenMask = cv.inRange(hsv, (26, 10, 30), (97, 100, 255))
+        # # hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
+        # # greenMask = cv.inRange(hsv, (26, 10, 30), (97, 100, 255))
 
-        # hsv[:,:,1] = greenMask
+        # # hsv[:,:,1] = greenMask
 
-        # back = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        im = cv.convertScaleAbs(im, alpha=1.5)
+        # # back = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        # im = cv.convertScaleAbs(im, alpha=1.5)
 
-        wpad = int(width / 50)
-        hpad = int(height / 50)
-        cropped_image = im[hpad : height - hpad, wpad : width - wpad]
-        thresh = cv.cvtColor(cropped_image.copy(), cv.COLOR_BGR2GRAY)
-        # cv.threshold(cropped_image,63,255,cv.THRESH_BINARY,thresh)
-        # thresh=cv.bitwise_not(thresh)
-        cv.adaptiveThreshold(
-            thresh,
-            255,
-            cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-            cv.THRESH_BINARY,
-            9,
-            2,
-            thresh,
-        )
-        contours, hierarchy = cv.findContours(
-            thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
-        )
-        area = []
-        for cnt in contours:
-            area.append(cv.contourArea(cnt))
-        area.sort(reverse=True)
+        # wpad = int(width / 50)
+        # hpad = int(height / 50)
+        # cropped_image = im[hpad : height - hpad, wpad : width - wpad]
+        # thresh = cv.cvtColor(cropped_image.copy(), cv.COLOR_BGR2GRAY)
+        # # cv.threshold(cropped_image,63,255,cv.THRESH_BINARY,thresh)
+        # # thresh=cv.bitwise_not(thresh)
+        # cv.adaptiveThreshold(
+        #     thresh,
+        #     255,
+        #     cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+        #     cv.THRESH_BINARY,
+        #     9,
+        #     2,
+        #     thresh,
+        # )
+        # contours, hierarchy = cv.findContours(
+        #     thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
+        # )
+        # area = []
+        # for cnt in contours:
+        #     area.append(cv.contourArea(cnt))
+        # area.sort(reverse=True)
 
-        edgecountavg = 0
-        counter = 0
-        for idx, cnt in enumerate(contours):
-            curarea = cv.contourArea(cnt)
-            if 1000 < curarea < width * height / 4:
-                approx = cv.approxPolyDP(
-                    cnt, 0.025 * cv.arcLength(cnt, True), True
-                )
-                edgecountavg += len(approx)
-                # print(cv.isContourConvex(approx))
-                # x, y, w, h = cv.boundingRect(
-                #     cnt
-                # )  # offsets - with this you get 'mask'
-                # cv.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                # # cv2.imshow('cutted contour',img[y:y+h,x:x+w])
-                # print(
-                #     "Average color (BGR): ",
-                #     np.array(cv.mean(im[y : y + h, x : x + w])).astype(
-                #         np.uint8
-                #     ),
-                # )
-                # cv.drawContours(im, contours, idx, (255, 0, 0), 1)
-                counter += 1
+        # edgecountavg = 0
+        # counter = 0
+        # for idx, cnt in enumerate(contours):
+        #     curarea = cv.contourArea(cnt)
+        #     if 1000 < curarea < width * height / 4:
+        #         approx = cv.approxPolyDP(
+        #             cnt, 0.025 * cv.arcLength(cnt, True), True
+        #         )
+        #         edgecountavg += len(approx)
+        #         # print(cv.isContourConvex(approx))
+        #         # x, y, w, h = cv.boundingRect(
+        #         #     cnt
+        #         # )  # offsets - with this you get 'mask'
+        #         # cv.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #         # # cv2.imshow('cutted contour',img[y:y+h,x:x+w])
+        #         # print(
+        #         #     "Average color (BGR): ",
+        #         #     np.array(cv.mean(im[y : y + h, x : x + w])).astype(
+        #         #         np.uint8
+        #         #     ),
+        #         # )
+        #         # cv.drawContours(im, contours, idx, (255, 0, 0), 1)
+        #         counter += 1
 
         # plt.imshow(cv.cvtColor(im, cv.COLOR_BGR2RGB))
         self._find_number()
@@ -90,7 +90,7 @@ class Card:
         self._find_color()
         self._find_fill()
 
-        edgecountavg = edgecountavg / counter
+        # edgecountavg = edgecountavg / counter
 
         # if edgecountavg < 5:
         #     self._shape = "diamond"
