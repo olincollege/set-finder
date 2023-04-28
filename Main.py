3662d002725a6f753218ab4109c088f0edbf76a0
Ari Bobesh <ari.bobesh@gmail.com>
2023-04-28 14:39:18 -0400

    Add v4l2py image capture

--- Main.py
+++ Main.py
@@ -3,13 +3,13 @@ from View import View
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    img = cv.imread("boards/1.jpg")
-    image = Image(img)
-    image.create_cards()
-    for card in image.cards:
-        fig = plt.figure()
-        plt.imshow(card._im)
-    # view=View(img)
-    # view.draw_set_cards()
-    # view.show()
+img = cv.imread("boards/1.jpg")
+image = Image(img)
+image.create_cards()
+image.find_sets()
+print(f"cards: {len(image.cards)},sets: {len(image.get_cards_set())}")
+for set in image.get_cards_set():
+    print(set)
+view = View(img)
+view.draw_set_cards(image.sets)
+view.show()
