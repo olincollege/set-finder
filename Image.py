15f18bf9c925552b1d51a4b03e7c9fabe58a2336
Ari Bobesh <ari.bobesh@gmail.com>
2023-04-30 19:47:38 -0400

    Add README template and docs in Card and Image

--- Image.py
+++ Image.py
@@ -1,3 +1,6 @@
+"""
+This module handles the global frame of the SET game and SET-finding.
+"""
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +10,12 @@ from Card import Card
 
 class Image:
     def __init__(self, img):
+        """
+        Initialize a new Image instance using a full board image.
+
+        Args:
+            img: a full board image of a SET game
+        """
         self.im = img
         self.simple_contours = []
         self.cards = []
@@ -14,6 +23,10 @@ class Image:
         self.cards_in_sets = set()
 
     def _find_card_contours(self):
+        """
+        Find all the contours in the image for potential cards using grayscale
+        and thresholding.
+        """
         imgray = cv.cvtColor(self.im, cv.COLOR_BGR2GRAY)
         cv.adaptiveThreshold(
             imgray,
@@ -38,12 +51,20 @@ class Image:
         )
 
     def _filter_contours_by_area(self):
+        """
+        Filter the detected contours by area to eliminate ones that are too
+        large and too small.
+
+        A contour that is greater than 1/12 of the image area is too large and
+        a contour that is smaller that 1/10 of the largest potential contour is
+        too small.
+        """
         height, width, _ = self.im.shape
         area = []
         for cnt in self.contours:
             area.append(cv.contourArea(cnt))
         area.sort(reverse=True)
-        minumum = 0
+        minimum = 0
         maximum = 0
         for index in range(len(area) - 1):
             if index > 30:
@@ -51,23 +72,34 @@ class Image:
             if area[index] >= (height * width) / 12:
                 maximum = index
             if area[maximum + 1] / area[index] < 10:
-                minumum = index
+                minimum = index
         self.contours = [
             cnt
             for cnt in self.contours
-            if (area[minumum] + 1 < cv.contourArea(cnt) < area[maximum] - 1)
+            if (area[minimum] + 1 < cv.contourArea(cnt) < area[maximum] - 1)
         ]
 
     def _filter_by_polygon(self):
+        """
+        Filter the contours by the ones that have four sides and are convex and
+        save the contour as a simplified four-point representation.
+        """
         for cnt in self.contours:
             approx = cv.approxPolyDP(cnt, 0.03 * cv.arcLength(cnt, True), True)
             if len(approx) == 4 and cv.isContourConvex(approx):
                 self.simple_contours.append(approx)
 
-    # def _crop(self, board_image, contours):
-    #     pass
-
     def _warp_perspective(self, card_contour):
+        """
+        Warp the perspective of the card to eliminate the effects of perspective
+        in the original image and attempt to rotate the card horizontally.
+
+        Args:
+            card_contour: the simplified four-point contour of the detected card
+
+        Returns:
+            An image of the card cropped and perspective-corrected.
+        """
         approx = np.float32([[item[0][0], item[0][1]] for item in card_contour])
         cardh = int(np.linalg.norm(approx[1] - approx[0]))
         cardw = int(np.linalg.norm(approx[2] - approx[1]))
@@ -78,11 +110,30 @@ class Image:
         return card
 
     def _rotate_card(self, cardw, cardh, card):
+        """
+        Rotate a card if it is sideways.
+
+        This method works a majority of the time, excepting highly-skewed
+        perspective scenarios.
+
+        Args:
+            cardw: width of the card in pixels
+            cardh: height of the card in pixels
+            card: image of the card before rotation
+
+        Returns:
+            A rotated version of the card if it is sideways; otherwise, returns
+            the non-rotated card.
+        """
         if cardh > cardw:
             return np.rot90(card)
         return card
 
     def create_cards(self):
+        """
+        Creates all detected Card instances after performing contour detection,
+        filtering, and perspective correction.
+        """
         self._find_card_contours()
         self._filter_contours_by_area()
         self._filter_by_polygon()
@@ -92,6 +143,10 @@ class Image:
             )
 
     def find_sets(self):
+        """
+        Find all SETs among the detected cards after filtering duplicates
+        through brute force and then add them to a list.
+        """
         self._deduplicate_cards()
         for index1, card1 in enumerate(self.cards):
             for index2, card2 in enumerate(self.cards):
@@ -110,17 +165,34 @@ class Image:
                             self.cards_in_sets.add(index3)
 
     def _deduplicate_cards(self):
+        """
+        Remove the duplicate cards detected based on equal comparative
+        representations.
+        """
         comparative = set()
         duplicates = []
         for index, card in enumerate(self.cards):
             if card.comparative in comparative:
                 duplicates.append(index)
             comparative.add(card.comparative)
-        print(f"Duplicate indices: {duplicates}")
         for index in sorted(duplicates, reverse=True):
             del self.cards[index]
 
     def _is_set(self, card1, card2, card3):
+        """
+        Compare three cards to detect if they form a SET.
+
+        The binary comparatives are compared logically as follows:
+        NOT (A XOR B XOR C XOR NOT (A OR B OR C))
+
+        Args:
+            card1: the first card to be compared
+            card2: the second card to be compared
+            card3: the third card to be compared
+
+        Returns:
+            A boolean representing if the three cards form a SET.
+        """
         return 0b000000000000 == ~(
             card1.comparative
             ^ card2.comparative
@@ -129,6 +201,9 @@ class Image:
         )
 
     def get_cards_nonset(self):
+        """
+        Returns the cards that are not in any SETs.
+        """
         return [
             card
             for index, card in enumerate(self.cards)
@@ -136,4 +211,7 @@ class Image:
         ]
 
     def get_cards_set(self):
+        """
+        Returns a list of 3-card lists representing the SETs discovered.
+        """
         return self.sets
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# python-project-template
-Template repository for a Python project.
+# Summary:
+This project uses CV in order to process a board of SET cards to find all
+possible SETs present.
+
+# Tips for Usage:
+- High-contrast background (preferably black)
+- No overlapping cards
+- Cards at least a pinky width apart
+- Take photo from as close as possible, leaving at least 1/4 of a card of margin
+around the edges
+- White, constant lighting (minimize shadows)
+- As little clutter/background noise as possible
+
+# Dependencies:
+Check requirements.txt for all dependencies, also listed below:
+- FILL THIS IN
+- ONCE WE DO THE .TXT
