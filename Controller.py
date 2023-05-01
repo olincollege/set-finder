d9d89b689a03dbcffac905ad3e1aa380fa71f878
= <=>
2023-04-30 20:18:43 -0400

    Change Controller.py to use v4l2py

--- Controller.py
+++ Controller.py
@@ -2,7 +2,9 @@
 Module to read webcam input
 """
 import cv2
-import Image
+import numpy as np
+from v4l2py import Device
+import matplotlib.pyplot as plt
 
 
 class Controller:
@@ -12,14 +14,22 @@ class Controller:
     Attributes:
         cam: The index of the camera to read from. 
     """
-    def __init__(self,index):
+    def __init__(self,index = 0):
         self.i = index
 
     def get_image(self):
         """
         Return a OpenCV array specifying an image at that time
         """
-        cam = cv2.VideoCapture(self.i)
-        _,frame = cam.read()
-        cam.release()
-        return frame 
+        with Device.from_id(self.i) as cam:
+            #Iterator to generator to get single value
+            frame = next(cam.__iter__())
+        return cv2.imdecode(np.frombuffer(frame.data, dtype=np.uint8),cv2.IMREAD_COLOR)
+
+if __name__ == "__main__":
+    # Colors will be not correct because of BGR encoding in OpenCV
+    control = Controller(0)
+    image = control.get_image()
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
