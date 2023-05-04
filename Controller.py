665a51b93246377c0dff7722fa892190b6eee573
= <=>
2023-05-04 16:38:12 -0400

    Refactor code into Controller.py from View.py

--- Controller.py
+++ Controller.py
@@ -1,41 +1,116 @@
 """
-Module to read webcam input
+Module to read user input
 """
+import math
 import cv2
 import numpy as np
-from v4l2py import Device
-import matplotlib.pyplot as plt
-
+from Image import Image
 
 class Controller:
     """
     Class for reading webcam input
-
-    Attributes:
-        cam: The index of the camera to read from.
     """
 
-    def __init__(self, index=0):
-        self.i = index
+    def __init__(self):
+        self.image = np.zeros((640,100,3), dtype=np.uint8)
 
+    def read_image(self,path):
+        self.image = cv2.imread(path)
+   
     def get_image(self):
+        return self._correct_color(self._resized_image())
+
+    def _resized_image(self):
         """
-        Return a OpenCV array specifying an image at that time
+        Resize the image
         """
-        cam = cv2.VideoCapture(
-            self.i,
-            apiPreference=cv2.CAP_ANY,
-            params=[cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY],
+        r = 480 / self.image.shape[0]
+        dim = (int(self.image.shape[1] * r), 480)
+        return cv2.resize(self.image, dim, interpolation=cv2.INTER_AREA)
+
+    def _correct_color(self,image):
+        """
+        Helper function to convert image color
+        """
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    def draw_nonset_cards(self, list_cards):
+        """
+        Draws cards on the image in black.
+
+        Args:
+            list_cards: A list of Card objects representing the non-SET cards.
+        """
+        for card in list_cards:
+            self._draw_rectangle(card, (255, 255, 255), 3)
+
+    def draw_set_cards(self, list_cards):
+        """
+        Draws cards on the image in green.
+
+        Args:
+            list_cards: A list of list of Card objects representing the SET
+            cards.
+        """
+        i = 1
+        card_counter = {}
+        for set in list_cards:
+            hsv_color = cv2.cvtColor(np.array([[[(30*i + math.sqrt(10*i))% 179,255,255]]],dtype=np.uint8), cv2.COLOR_HSV2BGR)
+            color = (
+                int(hsv_color[0][0][0]),
+                int(hsv_color[0][0][1]),
+                int(hsv_color[0][0][2]),
+            )
+            i += 1
+            for card in set:
+                self._draw_rectangle(card, color, card_counter.get(card, 0))
+                card_counter[card] = card_counter.get(card, 0) + 1
+
+    def _draw_rectangle(self, card, color, ring_count):
+        """
+        Draws a contour on the image, offset to not overlap other contours.
+
+        Args:
+            card: A Card object.
+            color: A tuple of the color to draw the rectangle.
+        """
+        contour_scale = (
+            (size := self.image.shape)[0] * size[1]
+        ) / cv2.contourArea(card.contour)
+        thickness = int(math.sqrt(size[0] * size[1]) / 50)
+        dilation_factor = 0.1 * ring_count * contour_scale / 30
+        x_com, y_com = np.average(card.contour, axis=0)[0]
+        trans = np.array([[1, 0, -x_com], [0, 1, -y_com], [0, 0, 1]])
+        untrans = np.array([[1, 0, x_com], [0, 1, y_com], [0, 0, 1]])
+        dilate = np.array(
+            [
+                [1 + dilation_factor, 0, 1],
+                [0, 1 + dilation_factor, 1],
+                [0, 0, 1],
+            ]
         )
-        _, frame = cam.read()
-        cam.release()
-        return frame
+        contour = np.r_[
+            np.transpose(np.reshape(np.array(card.contour), (4, 2), order="C")),
+            [[1, 1, 1, 1]],
+        ]
+        translated = np.matmul(trans, contour)
+        dilated = np.matmul(dilate, translated)
+        reverse_trans = np.matmul(untrans, dilated)
+        reshaped = np.reshape(
+            np.transpose(np.delete(reverse_trans, 2, axis=0)),
+            (4, 1, 2),
+            order="C",
+        ).astype(int)
+        cv2.drawContours(self.image, [reshaped], 0, color, thickness)
 
-
-if __name__ == "__main__":
-    # Colors will be not correct because of BGR encoding in OpenCV
-    control = Controller(0)
-    image = control.get_image()
-    plt.figure()
-    plt.imshow(image)
-    plt.show()
+    def generate_image_overlay(self):
+        print("Processing...")
+        im = Image(self.image)
+        print("finding cards")
+        im.create_cards()
+        print("finding sets")
+        im.find_sets()
+        print("drawing cards")
+        self.draw_nonset_cards(im.get_cards_nonset())
+        self.draw_set_cards(im.get_cards_set())
+        
--- View.py
+++ View.py
@@ -25,9 +25,8 @@ class View:
         image: A cv2.Mat type image showing the full board
     """
 
-    def __init__(self, image):
-        self._controller = Controller(0)
-        self.image = image
+    def __init__(self):
+        self._controller = Controller()
 
         # Create an instance of tkinter frame
         top = Tk()
@@ -37,7 +36,7 @@ class View:
         # Label(win, image= imgtk).pack()
         top.title("Toplevel 0")
         top.configure(highlightcolor="black")
-        self._set_gui_image()
+        self._set_gui_image(self._controller.get_image()) # creates self.image
         Label1 = tk.Label(top, image=self.img_gtk)  # Where image is inserted
         Label1.pack(fill=BOTH, expand=False, padx=10, pady=10, side=TOP)
         Label1.configure(activebackground="#f9f9f9")
@@ -68,101 +67,21 @@ class View:
         RunAgain.configure(text="Get File")
         self.button = RunAgain
 
-    def draw_nonset_cards(self, list_cards):
-        """
-        Draws cards on the image in black.
-
-        Args:
-            list_cards: A list of Card objects representing the non-SET cards.
-        """
-        for card in list_cards:
-            self._draw_rectangle(card, (255, 255, 255), 3)
-
-    def draw_set_cards(self, list_cards):
-        """
-        Draws cards on the image in green.
-
-        Args:
-            list_cards: A list of list of Card objects representing the SET
-            cards.
-        """
-        card_counter = {}
-        for set in list_cards:
-            color = (
-                random.randint(0, 255),
-                random.randint(0, 255),
-                random.randint(0, 255),
-            )
-            for card in set:
-                self._draw_rectangle(card, color, card_counter.get(card, 0))
-                card_counter[card] = card_counter.get(card, 0) + 1
-
-    def _draw_rectangle(self, card: Card, color, ring_count):
-        """
-        Draws a contour on the image, offset to not overlap other contours.
-
-        Args:
-            card: A Card object.
-            color: A tuple of the color to draw the rectangle.
-        """
-        contour_scale = (
-            (size := self.image.shape)[0] * size[1]
-        ) / cv2.contourArea(card.contour)
-        thickness = int(math.sqrt(size[0] * size[1]) / 50)
-        dilation_factor = 0.1 * ring_count * contour_scale / 30
-        x_com, y_com = np.average(card.contour, axis=0)[0]
-        trans = np.array([[1, 0, -x_com], [0, 1, -y_com], [0, 0, 1]])
-        untrans = np.array([[1, 0, x_com], [0, 1, y_com], [0, 0, 1]])
-        dilate = np.array(
-            [
-                [1 + dilation_factor, 0, 1],
-                [0, 1 + dilation_factor, 1],
-                [0, 0, 1],
-            ]
-        )
-        contour = np.r_[
-            np.transpose(np.reshape(np.array(card.contour), (4, 2), order="C")),
-            [[1, 1, 1, 1]],
-        ]
-        translated = np.matmul(trans, contour)
-        dilated = np.matmul(dilate, translated)
-        reverse_trans = np.matmul(untrans, dilated)
-        reshaped = np.reshape(
-            np.transpose(np.delete(reverse_trans, 2, axis=0)),
-            (4, 1, 2),
-            order="C",
-        ).astype(int)
-        cv2.drawContours(self.image, [reshaped], 0, color, thickness)
-
-    def new_image(self):
+    
+    def run(self):
         """'
         Run program again to capture image.
         """
         filename = filedialog.askopenfilename()
-        self.image = cv2.imread(filename)
-        print("Processing...")
-        im = Image(self.image)
-        print("finding cards")
-        im.create_cards()
-        print("finding sets")
-        im.find_sets()
-        print("drawing cards")
-        self.draw_nonset_cards(im.get_cards_nonset())
-        self.draw_set_cards(im.get_cards_set())
-        self.image = cv2.resize(self.image, (640, 480))
-        self._set_gui_image()
+        self._controller.read_image(filename)
+        self._controller.generate_image_overlay()
+        self._set_gui_image(self._controller.get_image())
         self.label.config(image=self.img_gtk)
         print("Done")
 
-    def _set_gui_image(self):
-        im = ImagePL.fromarray(self._correct_color())
-        self.img_gtk = ImageTk.PhotoImage(image=im)
-
-    def _correct_color(self):
-        """
-        Helper function to convert image color
-        """
-        return cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+    def _set_gui_image(self,image_cv2):
+        self.im_ref = ImagePL.fromarray(image_cv2)
+        self.img_gtk = ImageTk.PhotoImage(image=self.im_ref)
 
     def show(self):
         """
@@ -172,7 +91,7 @@ class View:
 
     def start_submit_thread(self):
         self.button["state"] = "disabled"
-        self.submit_thread = threading.Thread(target=self.new_image)
+        self.submit_thread = threading.Thread(target=self.run)
         self.submit_thread.daemon = True
         self.progress.start()
         self.submit_thread.start()
@@ -188,5 +107,5 @@ class View:
 
 if __name__ == "__main__":
     # Code to check if view is working
-    view = View(cv2.imread("boards/1.jpg"))
+    view = View()
     view.show()
